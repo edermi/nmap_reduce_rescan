5bdf56e3750e8986938478015a01e8814cc26751
Michael Eder <eder@hvs-consulting.de>
2024-06-27 21:01:27 +0000

    Some refactoring. Try to avoid duplicate scanning by filtering hosts only identified by a NetBIOS name and being more intelligent in mapping open ports to hosts for further scanning

--- main.go
+++ main.go
@@ -9,44 +9,75 @@ import (
 	"github.com/n0ncetonic/nmapxml"
 )
 
-var nmap_template string = "sudo nmap -sS -sU -Pn -T4 -A -v -n --open --max-retries 2 --host-timeout 3m --script-timeout 5m -p %s -oA %s %s\n"
+// Template for the generated scan string
+var nmap_template string = "sudo nmap -sS -sU -Pn -T4 -A -v -n --version-light --script \"default,safe,discovery\" --open --max-retries 2 --host-timeout 3m --script-timeout 5m -p %s -oA %s %s\n"
 var name_template string = "nmap_%s"
 
 func main() {
+	// Read the data
 	scanData, err := nmapxml.Readfile(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	// Store all hosts
+	known_hosts := make(map[string][]string)
+
+	// Iterate all hosts, collect port information and store it in the map
 	for _, host := range scanData.Host {
-		ports := make([]string, 0)
+		var hostname string
+
+		// Filter empty as well as short names without '.' (e.g. NetBIOS names)
+		if host.Hostnames.Hostname.Name != nil && !hostnameEmpty(*host.Hostnames.Hostname.Name) && strings.Contains(*host.Hostnames.Hostname.Name, ".") {
+			hostname = *host.Hostnames.Hostname.Name
+		} else if net.ParseIP(host.Address.Addr) != nil {
+			hostname = host.Address.Addr
+		} else {
+			continue
+		}
+
 		if host.Ports.Port == nil {
 			continue
 		}
 		for _, port := range *host.Ports.Port {
 			if port.State.State == "open" {
 				if port.Protocol == "tcp" {
-					ports = append(ports, fmt.Sprintf("T:%s", port.PortID))
+					addPort(known_hosts, hostname, fmt.Sprintf("T:%s", port.PortID))
 				} else {
-					ports = append(ports, fmt.Sprintf("U:%s", port.PortID))
+					addPort(known_hosts, hostname, fmt.Sprintf("U:%s", port.PortID))
 				}
 			}
 		}
-		if len(ports) > 0 {
-			portListStr := strings.Join(ports, ",")
-			if !strings.ContainsAny(*host.Hostnames.Hostname.Name, ".:") { // Ghetto check: omit NetBIOS names and stuff but keep IPv6
-				continue
-			}
-			if host.Hostnames.Hostname.Name != nil && !hostnameEmpty(*host.Hostnames.Hostname.Name) {
-				fmt.Printf(nmap_template, portListStr, fmt.Sprintf(name_template, *host.Hostnames.Hostname.Name), *host.Hostnames.Hostname.Name)
-			}
-			if net.ParseIP(host.Address.Addr) != nil {
-				fmt.Printf(nmap_template, portListStr, fmt.Sprintf(name_template, host.Address.Addr), host.Address.Addr)
-			}
-		}
-
 	}
 
+	// Iterate the map and create the relevant output
+	for hostname, ports := range known_hosts {
+		if len(ports) > 0 {
+			fmt.Printf(nmap_template, strings.Join(ports, ","), fmt.Sprintf(name_template, hostname), hostname)
+		}
+	}
+}
+
+func addPort(known_hosts map[string][]string, hostname string, port string) {
+	ports, exists := known_hosts[hostname]
+	if !exists {
+		// If the hostname does not exist, add it with the new port
+		known_hosts[hostname] = []string{port}
+	} else {
+		// Check if the port already exists to avoid duplicates
+		found := false
+		for _, p := range ports {
+			if p == port {
+				found = true
+				break
+			}
+		}
+		// If the port is not found, append it to the list of ports
+		if !found {
+			known_hosts[hostname] = append(ports, port)
+		}
+	}
 }
 
 func hostnameEmpty(hostname string) bool {
